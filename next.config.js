50f358f328e7d67560b8f0ac190c67106861af7a
Naoto Abe <naotoabe1@gmail.com>
2024-03-08 09:31:01 +0100

    edit config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/",
   output: "export",  // enable static exports
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
 }
 
 module.exports = nextConfig
